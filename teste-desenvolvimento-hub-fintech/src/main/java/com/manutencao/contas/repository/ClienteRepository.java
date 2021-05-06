faba072b958946bba037971519859a3c983b125d
Karol Dutra <ana.dutra1992@gmail.com>
2021-05-06 13:50:55 -0300

    teste

---
+++ teste-desenvolvimento-hub-fintech/src/main/java/com/manutencao/contas/repository/ClienteRepository.java
@@ -0,0 +1,12 @@
+package com.manutencao.contas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.manutencao.contas.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+
+}
---
+++ teste-desenvolvimento-hub-fintech/src/test/java/com/manutencao/contas/TesteDesenvolvimentoHubFintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manutencao.contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDesenvolvimentoHubFintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
